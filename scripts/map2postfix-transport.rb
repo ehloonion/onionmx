c723a08aafca44cbca5004f53dda9bb4b0fc44af
mh <mh@immerda.ch>
2017-01-07 13:33:02 +0100

    improve script based on feedback in the MR

--- scripts/map2postfix-transport.rb
+++ scripts/map2postfix-transport.rb
@@ -1,17 +1,35 @@
 #!/bin/env ruby
+#
+# Generates a sorted list of domains and the onion services they map to.
+# Sorting is done based on the onion service they map to, so that
+# domains delivered to the same onion service are grouped together.
+#
 
-map = ARGV[0] || File.expand_path(File.join(File.dirname(__FILE__),'..','sources','map.yml'))
+default_map = File.expand_path(File.join(File.dirname(__FILE__),'..','sources',
+                                         'map.yml'))
+maps = ARGV.empty? ? [ default_map ] : ARGV
 
-unless File.exists?(map)
-  STDERR.puts "No such map exists #{map}"
-  STDERR.puts "USAGE: #{$0} map.yaml"
-  exit 1
+maps.each do |map|
+  unless File.exists?(map)
+    STDERR.puts "No such map exists #{map}"
+    STDERR.puts "USAGE: #{$0} map.yaml [ map2.yaml [ map3.yaml ] ]"
+    exit 1
+  end
 end
 
 require 'yaml'
 
-ymap = YAML.load_file(map)
+merged_maps = maps.inject({}){|res,map| res.merge(YAML.load_file(map)) }
 
-longest_domain_length = ymap.values.collect{|e| Array(e) }.flatten.sort{|a,b| a.length <=> b.length }.last.length
+all_domains = merged_maps.values.collect{|e| Array(e) }.flatten
+longest_domain_length = (all_domains.sort do |a,b|
+  a.length <=> b.length
+end).last.length
 
-puts ymap.keys.sort.collect{|os| Array(ymap[os]).sort.collect{|d| "%-#{longest_domain_length}s smtptor:[%s]" % [d,os] } }.flatten
+output = (merged_maps.keys.sort.collect do |os|
+  Array(merged_maps[os]).sort.collect do |d|
+    "%-#{longest_domain_length}s smtptor:[%s]" % [d,os]
+  end
+end).flatten
+
+puts output
